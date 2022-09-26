4613f44429e2c3247c163a40a68f287d363d1dba
Ruslan Khaydarov <rushaidarov@yandex.ru>
2022-09-26 11:37:23 +0300

    deploy3

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     '*',
     'localhost',
-    'firstprojectyap.hopto.org',
+    'firstprojectyap.hopto.org'
 ]
 
 
@@ -128,4 +128,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
